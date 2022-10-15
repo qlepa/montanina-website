32277ba084e534bd1d70c6d59ab77dd7445a2d61
qlepa <t.klepacki@gmail.com>
2022-10-16 01:08:44 +0200

    Add menu items wip

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useScrollPosition } from "../hooks/useScrollPosition";
+import logo from '../assets/logo.jpg';
 
 function NavBar() {
   const [isShrunk, setShrunk] = useState(false);
@@ -9,13 +10,19 @@ function NavBar() {
 
   return (
     <nav className={`fixed top-0 flex justify-center items-center border border-cyan-900 ${isShrunk ? 'h-[50px]' : 'h-[150px]'} w-[1280px] duration-1000`}>
-      <div className="border border-red-800">
-        Logo
+      <div className="flex items-center flex-initial border border-red-800 h-24">
+        <img src={logo} className="h-full"></img>
+        ASD Polisportiva Montanina
       </div>
-      <div className="border border-yellow-600">
-        Menu List
+      <div className="grow-[2] shrink-0 border border-yellow-600">
+        <ul className="flex justify-between">
+          <li>Squadra</li>
+          <li>Storia</li>
+          <li>Community</li>
+          <li>Calendar</li>
+        </ul>
       </div>
-      <div className="border border-purple-900">
+      <div className="grow-[1] shrink-0 border border-purple-900">
         Next Match
       </div>
     </nav>
